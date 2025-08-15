4cb67d1257860f98cdae1f9e404479db8f31c2ef
pavan44444 <pk4711560@gmail.com>
2025-08-15 08:44:01 +0530

    check

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', // your MySQL password
-    database: 'PRIMARYKEY',
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10
 });
---
+++ public/config.js
@@ -0,0 +1,2 @@
+// config.js
+const API_BASE_URL = 'https://primarykey-production.up.railway.app';
