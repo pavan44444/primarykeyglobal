432abfcbd9b31f9f43603c785ca244f8534d53d6
pavan44444 <pk4711560@gmail.com>
2025-08-15 11:30:27 +0530

    final one

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
+// db.js
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // Load variables from .env
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', // your MySQL password
-    database: 'PRIMARYKEY',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306, // Default MySQL port
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'PRIMARYKEY',
     waitForConnections: true,
     connectionLimit: 10
 });
